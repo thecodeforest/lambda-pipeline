2cadb7d1b5b3d3912af0c8bba00ddee5644d22f8
Mark LeBoeuf <mark.leboeuf@nike.com>
2023-02-16 14:58:56 -0800

    breaking

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ openweather_api_key = os.environ["OPENWEATHER_API_KEY"]
 with open(Path(__file__).parent / "conf.yaml") as f:
     conf = yaml.load(f, Loader=yaml.FullLoader)
 
-
+print(gonna_break_it)
 def send_run_details(data: pd.DataFrame, topic_name: str, run_time: float, validation_flag: bool) -> None:
     sns = boto3.client('sns')
     response = sns.list_topics()
